859950b4307314baedc8bb8d7fb7ee0740505bdb
unknown <fdlcm974@gmail.com>
2021-06-25 14:35:20 +0200

    modification du formulaire SignUpForm et du template

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
@@ -137,3 +137,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
