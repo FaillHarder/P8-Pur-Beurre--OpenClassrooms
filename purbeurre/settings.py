0abd5c3329fa8f8d51ef2ce5bf8d28613cc2be07
unknown <fdlcm974@gmail.com>
2021-07-31 22:36:30 +0200

    --amend

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -139,12 +139,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/dist"),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
