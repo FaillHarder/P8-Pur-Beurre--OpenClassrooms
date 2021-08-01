dad4c0d1cffbca8e62ce726af565ea4756d9ff04
unknown <fdlcm974@gmail.com>
2021-08-01 17:06:12 +0200

    --amend

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -150,5 +150,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = '/'
 
 django_heroku.settings(locals())
